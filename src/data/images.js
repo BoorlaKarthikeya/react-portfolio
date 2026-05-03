1bdbeef084d5522da26db2ffd51a7aaaf0047a3c
kboorla_sfemu <kboorla@salesforce.com>
2026-05-03 14:55:22 +0530

    updating profile image

--- src/data/images.js
+++ src/data/images.js
@@ -1,5 +1,5 @@
 // Profile
-export { default as profileImg } from "../assets/karthikeyaProfile2.jpg";
+export { default as profileImg } from "../assets/KarthikeyaProfile.jpeg";
 
 // Experience logos
 export { default as cbitLogo } from "../assets/cbit.jpeg";
